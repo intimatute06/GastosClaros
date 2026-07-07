aa50a8bef248d90c97dc9d5b382c0f840bb52acb
intimatute06 <matuteinti@gmail.com>
2026-07-06 23:20:21 -0500

    feat: API Gateway con YARP, enruta REST y Swagger hacia Backend Core

---
+++ api-gateway/ApiGateway/Program.cs
@@ -0,0 +1,12 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.MapGet("/", () => "GastosClaros API Gateway - activo");
+
+app.MapReverseProxy();
+
+app.Run();
