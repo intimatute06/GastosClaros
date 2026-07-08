9a62c5bd8763d1cee72a47a48520ae6c8c13ed78
intimatute06 <matuteinti@gmail.com>
2026-07-07 19:53:14 -0500

    feat: CORS en API Gateway para SwaggerHub, documentacion publicada

--- api-gateway/ApiGateway/Program.cs
+++ api-gateway/ApiGateway/Program.cs
@@ -3,7 +3,19 @@
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("SwaggerHub", policy =>
+    {
+        policy.WithOrigins("https://app.swaggerhub.com")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("SwaggerHub");
 
 app.MapGet("/", () => "GastosClaros API Gateway - activo");
 
